84bca8bba143d9d8c732d050bc189da99b18da9e
Frederico Reid <fredericoreid@ibm.com>
2021-06-02 10:09:55 -0300

    Criado os primeiros testes da aplicação

--- src/test/java/com/carros/CarrosApplicationTests.java
+++ src/test/java/com/carros/CarrosApplicationTests.java
@@ -1,13 +1,52 @@
 package com.carros;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.carros.domain.Carro;
+import com.carros.domain.CarroService;
+import com.carros.domain.dto.CarroDTO;
 
 @SpringBootTest
 class CarrosApplicationTests {
+	
+	@Autowired
+	private CarroService service;
 
 	@Test
-	void contextLoads() {
+	public void test1() {
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("esportivos");
+		
+		CarroDTO c = service.insert(carro);
+		
+		assertNotNull(c);
+		
+		Long id = c.getId();
+		assertNotNull(id);
+		
+		//Buscar o objeto
+		Optional<CarroDTO>op = service.getCarrosById(id);
+		assertTrue(op.isPresent());
+		
+		c = op.get();
+		assertEquals("Ferrari", c.getNome());
+		assertEquals("esportivos", c.getTipo());
+		
+		//Deletar o objeto
+		service.delete(id);
+		
+		//Verificar se deletou
+		assertFalse(service.getCarrosById(id).isPresent());
 	}
 
 }
